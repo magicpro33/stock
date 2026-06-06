e00eae78627e1edebcb341f3171ea335c2bf3021
AIupscale <citizen227478@gmail.com>
2026-06-05 20:38:52 -0400

    Revert "Add files via upload"
    This reverts commit b600b10bc82d36ccba20512d3940398c6127ffa8.

--- house_finder/__init__.py
+++
@@ -1,5 +0,0 @@
-"""House search by zip code, age, and estimated value."""
-
-from house_finder.models import House
-
-__all__ = ["House"]
--- run_app.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-if not exist .venv\Scripts\streamlit.exe (
-  call install.bat
-)
-.venv\Scripts\streamlit.exe run app.py %*
